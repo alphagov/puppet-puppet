8fda6a78e1b7af19ed1827c11f9a8a5cb58113e0
Anna Shipman <anna@annashipman.co.uk>
2013-08-19 10:16:17 +0100

    Make Puppet-common depend on Facter
    Puppet-common will pull in Facter as a dependency but a different version, and
    then pull in the correct version. Making Puppet-common depend on Facter avoids
    this.

--- manifests/package.pp
+++ manifests/package.pp
@@ -4,14 +4,13 @@ class puppet::package {
   $puppet_ensure = $::puppet::puppet_ensure
   $facter_ensure = $::puppet::facter_ensure
 
+  package { 'facter':
+    ensure => $facter_ensure,
+  } ->
   package { 'puppet-common':
     ensure => $puppet_ensure,
   } ->
   package { 'puppet':
     ensure => $puppet_ensure,
-  }
-
-  package { 'facter':
-    ensure => $facter_ensure,
   }
 }

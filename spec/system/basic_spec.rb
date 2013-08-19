9e1dd1145c4143b175e44287f3abccf075279bf1
Anna Shipman <anna@annashipman.co.uk>
2013-08-19 10:03:05 +0100

    Add idempotency test to system tests

--- spec/system/basic_spec.rb
+++ spec/system/basic_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper_system'
 
 describe 'basic tests' do
-  it 'class should work without errors' do
+  it 'class should work without errors and be idempotent' do
     pp = <<-EOS
       class { 'puppet': }
     EOS
 
     puppet_apply(pp) do |r|
       r.exit_code.should == 2
+      r.refresh
+      r.exit_code.should be_zero
     end
   end
 end

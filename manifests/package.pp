7ed81f9271d9f0624100b47dead5077239f4b48e
Anna Shipman <anna@annashipman.co.uk>
2013-08-19 10:10:57 +0100

    Make Puppet dependent on Puppet-common

--- manifests/package.pp
+++ manifests/package.pp
@@ -4,11 +4,10 @@ class puppet::package {
   $puppet_ensure = $::puppet::puppet_ensure
   $facter_ensure = $::puppet::facter_ensure
 
-  package { 'puppet':
-    ensure => $puppet_ensure,
-  }
-
   package { 'puppet-common':
+    ensure => $puppet_ensure,
+  } ->
+  package { 'puppet':
     ensure => $puppet_ensure,
   }
 
